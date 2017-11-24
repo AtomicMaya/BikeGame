2f2d5f80220c29be762cce75604d0945d7b0e5c0
Lijwent <clement.jeannet@epfl.ch>
2017-11-24 18:00:03 +0100

    27

--- src/main/game/actor/myEntities/EntityBuilder.java
+++ src/main/game/actor/myEntities/EntityBuilder.java
@@ -11,22 +11,57 @@ import java.awt.*;
 
 public class EntityBuilder {
 
+    /**
+     * Create and add an ImageGraphics to an entity
+     *
+     * @param entity The entity
+     * @param imagePath The path to the image to add
+     * @param width The width of the image
+     * @param height The height of the image
+     * @return A new ImageGraphics associated to the entity
+     * */
     public static ImageGraphics addGraphics(Entity entity, String imagePath, float width, float height){
         ImageGraphics ig = new ImageGraphics(imagePath, width, height);
         ig.setParent(entity);
         return ig;
     }
 
-
+    /**
+     * Create and add a ShapeGraphics to an entity
+     *
+     * @param entity The entity
+     * @param shape shape, may be null
+     * @param fillColor fill color, may be null
+     * @param outlineColor outline color, may be null
+     * @param thickness outline thickness
+     * @param alpha transparency, between 0 (invisible) and 1 (opaque)
+     * @param depth render priority, lower-values drawn first
+     * @return The ShapeGraphics created and associated to the image
+     */
     public static ShapeGraphics addGraphics(Entity entity, Shape shape, Color fillColor, Color outlineColor, float thickness, float alpha, float depth){
         ShapeGraphics s = new ShapeGraphics(shape, fillColor, outlineColor, thickness, alpha, depth);
         s.setParent(entity);
         return s;
     }
-
+    /**
+     * Build the entity, which give a physical representation for the engine
+     *
+     * @param entity The entity
+     * @param shape The shape to give to the entity
+     */
     public static void build(Entity entity, Shape shape){
         build(entity, shape, -1, -1, false);
     }
+
+    /**
+     * Build the entity, which give a physical representation for the engine
+     *
+     * @param entity The entity
+     * @param shape The shape to give to the entity
+     * @param friction friction to give to the entity, if negative -> default value
+     * @param density density of the entity, if negative -> default value
+     * @param ghost whether this part is hidden and act only as a sensor
+     */
     public static void build(Entity entity, Shape shape, float friction, float density, boolean ghost){
         PartBuilder partBuilder = entity.createPartBuilder();
         partBuilder.setShape(shape);
