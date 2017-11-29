8ce5be3dd8b2502bb0e0fa07cd7d411bea9b3fcf
N <boeckh.nicolas@gmail.com>
2017-11-29 15:01:13 +0100

    38.2 - Fix second Merge Shit

--- src/main/game/actor/QuickMafs.java
+++ src/main/game/actor/QuickMafs.java
@@ -2,9 +2,6 @@ package main.game.actor;
 
 import main.math.Vector;
 
-/**
- * Created on 11/29/2017 at 11:09 AM.
- */
 public class QuickMafs {
 	public static float getDistance(Vector one, Vector two) {
 		return (float) Math.sqrt(Math.pow(one.x - two.x, 2) + Math.pow(one.y - two.y, 2));
